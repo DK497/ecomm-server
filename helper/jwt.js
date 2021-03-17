91fa407e0baa81bc5aa93777c445d1b1fbadad4d
DK497 <dhananjaik497@gmail.com>
2021-03-17 17:33:57 +0530

    jwt22

--- helper/jwt.js
+++ helper/jwt.js
@@ -14,7 +14,7 @@ function authjwt(){
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/users\/.*/,methods: ['GET']},
-            { url: /\/api\/v1\/orders/,methods: ['POST','GET']},
+            { url: /\/api\/v1\/orders/,methods: ['POST']},
             `${api}/users/login`,
             `${api}/users/register`,
         ]  
